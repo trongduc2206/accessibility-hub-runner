bc1a61a9c685f4ada022dd35972147cd312106ee
Trong Duc <trongduc22062000@gmail.com>
2025-05-18 16:26:37 +0300

    Exit if either axe or pa11y finds issues

--- run-axe.sh
+++ run-axe.sh
@@ -15,11 +15,26 @@ for TOOL in "${TOOLS_ARRAY[@]}"; do
     if [ "$TOOL" == "axe" ]; then
         echo "Running Axe CLI on $URL with axe-source $GITHUB_ACTION_PATH/axe.js with rules: $RULE_IDS"
         axe "$URL" --chrome-options="headless,disable-gpu,no-sandbox,incognito" --chrome-path="$CHROME_PATH" --rules "$RULE_IDS" --axe-source "$GITHUB_ACTION_PATH/axe.js" --verbose --exit
+        if [ $? -ne 0 ]; then
+            echo "Axe CLI reported issues."
+            FAILED=1
+        fi
     elif [ "$TOOL" == "pa11y" ]; then
         IGNORED_RULES=$(curl -s "https://accessibility-hub-be.onrender.com/ignore-pa11y-rules/$SERVICE_ID")
         echo "Running Pa11y CLI on $URL with ignored rules: $IGNORED_RULES"
         pa11y "$URL" --config "$GITHUB_ACTION_PATH/pa11y-config.json" --ignore "$IGNORED_RULES"
+        if [ $? -eq 2 ]; then
+            echo "Pa11y CLI reported issues."
+            FAILED=1
+        fi
     else
         echo "Tool $TOOL is not supported. Skipping..."
     fi
 done
+
+if [ $FAILED -ne 0 ]; then
+    echo "One or more tools reported issues. Failing the workflow."
+    exit 1
+fi
+
+echo "All tools executed successfully with no issues."
